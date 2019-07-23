d0c88a07702dbf767f7341686fdf4fc980cd22eb
yagami light <halysl0817@gmail.com>
2019-07-23 10:15:36 +0800

    add 更新目录

--- _posts/ref_all_post.py
+++ _posts/ref_all_post.py
@@ -4,7 +4,10 @@ import os
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(dir_path, 'post_name'), 'w') as f:
+    count = 0
     for file_name in os.listdir(dir_path):
-        if not os.path.isdir(file_name) and "md" in file_name:
+        if not os.path.isdir(file_name) and "md" in file_name and "template" not in file_name:
+            count += 1
             f.write(file_name)
             f.write("\n")
+    f.write(f"\n总计有{count}篇文章")
