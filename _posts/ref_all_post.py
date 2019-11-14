2eba5c9b9656f8931184dbf6ba74477e33f82e19
Yagami Light <halysl0817@gmail.com>
2019-11-14 16:40:03 +0800

    add modified: 使用pathlib重写文章列表获取脚本

--- _posts/ref_all_post.py
+++ _posts/ref_all_post.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-import os
+import re
+from pathlib import Path
 
-dir_path = os.path.dirname(os.path.abspath(__file__))
+base_path = Path(__file__).absolute().parent
+post_name_path = base_path.joinpath('post_name')
+all_md_file_path = base_path.cwd().glob('*.md')
 
-with open(os.path.join(dir_path, 'post_name'), 'w') as f:
+all_md_file_name = [i.name for i in all_md_file_path]
+all_md_file_name = sorted(filter(lambda x: re.match(r"\d{4}-\d{2}-\d{2}-.*.md", x), all_md_file_name))
+
+with open(post_name_path, 'w') as f:
     count = 0
-    for file_name in os.listdir(dir_path):
-        if not os.path.isdir(file_name) and "md" in file_name and "template" not in file_name:
+    for file_name in all_md_file_name:
             count += 1
             f.write(file_name)
             f.write("\n")
